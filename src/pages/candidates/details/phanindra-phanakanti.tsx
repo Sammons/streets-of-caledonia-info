6c56f0377c6ced197fd877ff452bf209a50b217c
Ben Sammons <ben@sammons.io>
2024-02-13 14:43:49 -0600

    Update phanindra-phanakanti.tsx typo in welfare

--- src/pages/candidates/details/phanindra-phanakanti.tsx
+++ src/pages/candidates/details/phanindra-phanakanti.tsx
@@ -8,7 +8,7 @@ const candidateInfo = {
   name: "Phanindra Phanakanti",
   edu: "Bachelors in Computer Sciences and PMP Certified",
   background: "I have a strong background in community engagement. Have been awarded multiple \"Force for Good\" awards for fostering positive relationships within our neighborhood. With extensive experience in project management and a commitment to open communication, I aim to contribute to the HOA by ensuring effective governance and addressing the needs of our community.",
-  comments: "Passionate about community wellfare, I believe my dedication and collaborative approach make me an ideal candidate for the board. I am committed to transparent decision-making and fostering a sense of unity among house owners.",
+  comments: "Passionate about community welfare, I believe my dedication and collaborative approach make me an ideal candidate for the board. I am committed to transparent decision-making and fostering a sense of unity among house owners.",
   image: '../../../images/placeholder-photo.svg'
 }
 
